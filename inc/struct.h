20dca3e77c154841f7f32b2271a8774923c06a0a
Jocelyn Piquet <jpiquet@z3r10p5.42lyon.fr>
2025-05-22 13:59:26 +0200

    added .h

--- inc/struct.h
+++ inc/struct.h
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpiquet <jocelyn.piquet1998@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 12:59:49 by jpiquet           #+#    #+#             */
-/*   Updated: 2025/05/22 13:49:39 by jpiquet          ###   ########.fr       */
+/*   Created: 2025/05/22 13:50:00 by jpiquet           #+#    #+#             */
+/*   Updated: 2025/05/22 13:54:09 by jpiquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-#define MINISHELL_h
+#ifndef STRUC_H
+#define STRUC_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <limits.h>
-
-
+typedef struct s_cmd
+{
+	char	*pathname;
+	char	**args;
+	int		infile;
+	int		outfile;
+	bool	valid;
+	s_cmd	*next;
+	
+}t_cmd;
 
 #endif
