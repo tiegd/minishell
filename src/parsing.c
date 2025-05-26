11cc4c55948b738f9a5c348af12f87b11273f00c
Gautier Ducurtil <gaducurt@z3r10p6.42lyon.fr>
2025-05-26 09:06:02 +0200

    adding -lreadline in Makefile

--- src/parsing.c
+++ src/parsing.c
@@ -6,23 +6,23 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:03:53 by gaducurt          #+#    #+#             */
-/*   Updated: 2025/05/26 08:30:52 by gaducurt         ###   ########.fr       */
+/*   Updated: 2025/05/26 08:56:49 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-
+#include "minishell.h"
 
 int	ft_check_prompt(char *input)
 {
 	int		i;
 	char	**prompt;
 
+	i = 0;
 	prompt = ft_multi_split(input, ' ', '\t');
 	while (prompt[i])
 	{
-		printf("prompt[%d] = %s\n", prompt[i]);
+		printf("prompt[%d] = %s\n", i, prompt[i]);
 		i++;
 	}
 	return (1);
