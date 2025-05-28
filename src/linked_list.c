a02be8be633504fb1af9341f9a267ab8ec2294e1
Gautier Ducurtil <gaducurt@z3r10p6.42lyon.fr>
2025-05-28 17:30:49 +0200

    conditional jump in ft_lst_addback

--- src/linked_list.c
+++ src/linked_list.c
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 13:01:04 by gaducurt          #+#    #+#             */
-/*   Updated: 2025/05/28 15:34:35 by gaducurt         ###   ########.fr       */
+/*   Updated: 2025/05/28 16:47:32 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,10 @@ void	ft_lst_addback(t_token *lst, char *s, int len)
 	t_token	*last;
 	int		i;
 
-	new = malloc(sizeof(t_list));
+	new = malloc(sizeof(t_token));
 	if (!new)
 		return (ft_lstfree(lst));
-	new->content = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	new->content = malloc((len + 1) * sizeof(char));
 	if (!new->content)
 	{
 		free(new);
@@ -65,26 +65,31 @@ void	ft_lst_addback(t_token *lst, char *s, int len)
 	{
 		new->content[i] = s[i];
 		printf(YELLOW"new->content[i] = %c\n"RESET, new->content[i]);
+		printf("i boucle = %d\n", i);
 	}
 	new->content[i] = '\0';
 	new->next = NULL;
-	printf(GREEN"new->content = %s\n"RESET, new->content);
-	printf("continue\n");
+	printf(YELLOW"new->content[i] = %c\n"RESET, new->content[i]);
+	printf("i boucle = %d\n", i);
 	if (!lst)
 	{
 		lst = new;
 		return ;
 	}
-	last = ft_lst_last(lst);
-	last->next = new;
+	else
+	{
+		last = ft_lst_last(lst);
+		last->next = new;
+	}
+	printf(GREEN"new->content = %s\n"RESET, new->content);
+	printf("continue\n");
 }
 
 // Just for test the list. This function will be deleted.
 
-void	ft_print_lst(t_token *lst, int len_tab)
+void	ft_print_lst(t_token *lst)
 {
-	(void)len_tab;
-	while (lst->next)
+	while (lst)
 	{
 		printf(RED"content = %s\n"RESET, lst->content);
 		lst = lst->next;
@@ -107,8 +112,8 @@ t_token	*ft_tab_to_lst(char **prompt, int len_tab)
 		printf(CYAN"len = %d\n"RESET, len);
 		ft_lst_addback(lst, prompt[i], len);
 		i++;
-		printf("i = %d", i);
+		printf("i = %d\n", i);
 	}
-	ft_print_lst(lst, len_tab);
+	ft_print_lst(lst);
 	return (lst);
 }
