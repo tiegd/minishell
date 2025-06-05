386125ec8d171fda398416368a7d7301093aebec
Gautier Ducurtil <gaducurt@z3r10p4.42lyon.fr>
2025-06-05 16:28:24 +0200

    pipex.c has been modified

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 10:51:32 by gaducurt          #+#    #+#             */
-/*   Updated: 2025/06/05 15:23:53 by gaducurt         ###   ########.fr       */
+/*   Updated: 2025/06/05 16:06:27 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,3 @@ void	pipex(t_cmd *cmd, char *envp[], t_fd fd)
 	first_pipe(cmd, envp, &fd, 0);
 	last_pipe(cmd, envp, &fd, 1);
 }
-    
