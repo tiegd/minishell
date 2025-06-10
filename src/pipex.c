976e73d69936be5df0c287419fcb3d7ce8ed139a
Gautier Ducurtil <gaducurt@z1r11p4.42lyon.fr>
2025-06-10 13:33:57 +0200

    one_pipe and multi_pipe

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 10:51:32 by gaducurt          #+#    #+#             */
-/*   Updated: 2025/06/06 14:48:13 by gaducurt         ###   ########.fr       */
+/*   Updated: 2025/06/10 10:51:18 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,16 @@ static void	last_pipe(t_cmd *cmd, char *envp[], t_fd *fd, int i)
 	return ;
 }
 
-void	exec_pipex(t_cmd *cmd, char *envp[], t_fd fd)
+void	exec_one_pipex(t_cmd *cmd, char *envp[], t_fd fd)
 {
 	first_pipe(cmd, envp, &fd, 0);
 	last_pipe(cmd, envp, &fd, 1);
-	
+}
+
+void	exec_multi_pipex(t_cmd *cmd, char *envp[], t_fd fd)
+{
+	first_pipe(cmd, envp, &fd, 0);
+	last_pipe(cmd, envp, &fd, 1);
 }
 
 void	pipex(t_token *lst, t_cmd *cmd, char **env)
@@ -75,10 +80,10 @@ void	pipex(t_token *lst, t_cmd *cmd, char **env)
 	ft_fill_index(lst);
 	if (nb_pipe == 1)
 	{
-		
+		exec_one_pipex(cmd, env, fd);
 	}
 	if (nb_pipe > 1)
 	{
-		
+		exec_multi_pipex(cmd, env, fd);
 	}
 }
