702d5c65beb2a28c29d6018752342b5a6c99acc9
Gautier Ducurtil <gaducurt@z1r10p4.42lyon.fr>
2025-06-11 11:24:51 +0200

    comment ft_fill_index

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 10:51:32 by gaducurt          #+#    #+#             */
-/*   Updated: 2025/06/11 11:11:00 by gaducurt         ###   ########.fr       */
+/*   Updated: 2025/06/11 11:24:12 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,31 @@ int	ft_count_pipe(t_token *lst)
 	count = 0;
 	while (tmp->next)
 	{
-		if (tmp->type == '|')
+		if (tmp->type == PIPE)
 			count++;
 		tmp = tmp->next;
 	}
 	return (count);
 }
 
-// Fill the index of each token in lst. Index is 1 afeter a pipe.
+// // Fill the index of each token in lst. Index is 1 afeter a pipe.
 
-void	ft_fill_lst(t_token *lst)
-{
-	t_token	*tmp;
-	int		index;
+// void	ft_fill_index(t_token *lst)
+// {
+// 	t_token	*tmp;
+// 	int		index;
 	
-	tmp = lst;
-	index = 1;
-	while (tmp->next)
-	{
-		if (tmp->type == '|')
-				index = 0;
-		tmp->index = index;
-		index++;
-		tmp = tmp->next;
-	}
-}
+// 	tmp = lst;
+// 	index = 1;
+// 	while (tmp->next)
+// 	{
+// 		if (tmp->type == PIPE)
+// 				index = 0;
+// 		tmp->index = index;
+// 		index++;
+// 		tmp = tmp->next;
+// 	}
+// }
 
 void	ft_init_fd(t_token *lst)
 {
@@ -103,18 +103,23 @@ void	exec_multi_pipex(t_cmd *cmd, char *envp[], t_fd fd)
 
 void	pipex(t_token *lst, t_cmd *cmd, char **env)
 {
-	int	nb_pipe;
 	t_fd	fd;
+	int		nb_pipe;
+	int		i;
 
 	nb_pipe = ft_count_pipe(lst);
 	ft_fill_index(lst);
 	ft_open_fd(lst, &fd);
-	if (nb_pipe == 1)
+	while (i != nb_pipe)
 	{
-		exec_one_pipex(cmd, env, fd);
-	}
-	if (nb_pipe > 1)
-	{
-		exec_multi_pipex(cmd, env, fd);
+		if (nb_pipe == 1)
+		{
+			exec_one_pipex(cmd, env, fd);
+		}
+		if (nb_pipe > 1)
+		{
+			exec_multi_pipex(cmd, env, fd);
+		}
+		i++;
 	}
 }
