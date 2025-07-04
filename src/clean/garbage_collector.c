d91c9d2830e4c2208aff788d6fb45092f78bed3b
Jocelyn Piquet <jpiquet@z1r10p5.42lyon.fr>
2025-07-04 09:55:55 +0200

    added gmalloc before changes

--- src/clean/garbage_collector.c
+++ src/clean/garbage_collector.c
@@ -6,7 +6,7 @@
 /*   By: jpiquet <jocelyn.piquet1998@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 14:34:27 by jpiquet           #+#    #+#             */
-/*   Updated: 2025/07/03 16:53:00 by jpiquet          ###   ########.fr       */
+/*   Updated: 2025/07/04 09:41:46 by jpiquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,25 @@ void	gb_free_all(t_gmalloc **head)
 
 void	gfree(t_gmalloc **head, void *ptr)
 {
-	t_gmalloc *temp;
+	t_gmalloc	*temp;
+	t_gmalloc	*prev;
 
 	temp = *head;
+	prev = NULL;
+	if (*head == NULL)
+		return ;
 	while (temp)
 	{
 		if (temp->memory == ptr)
+		{
+			if (prev == NULL)
+				*head = temp->next;
+			else
+				prev->next = temp->next;
 			free(temp->memory);
+			free(temp);
+		}
+		prev = temp;
 		temp = temp->next;
 	}
 }
@@ -70,13 +82,19 @@ void	*gb_malloc(size_t size, t_gmalloc *lst)
 	void		*memory;
 	t_gmalloc	*new;
 
-	memory = malloc(sizeof(size));
+	memory = malloc(size);
 	if (!memory)
 	{
-		
 		gb_free_all(&lst);
+		exit(1);
 	}
 	new = malloc(sizeof(t_gmalloc));
+	if (!new)
+	{
+		free(memory);
+		gb_free_all(&lst);
+		exit(1);
+	}
 	new->memory = memory;
 	gmalloc_add_back(&lst, new);
 	return (memory);
